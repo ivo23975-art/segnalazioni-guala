13dd02db5ef0a90b27b72a5ba4030b77e80557a6
ivo23975-art <ivo23975@gmail.com>
2025-11-28 02:07:43 +0100

    Update service-worker.js
    agg

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,22 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>clients.claim());
+self.addEventListener("install", e => {
+  console.log("Service Worker: Install");
+  e.waitUntil(
+    caches.open("app-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
