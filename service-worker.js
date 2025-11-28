01d80a43444af235f396a13d3b1dee2bef39cc4c
ivo23975-art <ivo23975@gmail.com>
2025-11-28 02:36:55 +0100

    Update service-worker.js
    AGG

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,39 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker: Install");
-  e.waitUntil(
-    caches.open("app-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
-    })
+const CACHE_NAME = "guala-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192-IB.png",
+  "./icon-512-IB.png"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+// ATTIVA NUOVA CACHE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// RISPOSTA OFFLINE
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
