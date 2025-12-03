30b46cb56bad136848a3e00cccf8a416995dc6c9
ivo23975-art <ivo23975@gmail.com>
2025-12-03 19:28:41 +0100

    Update service-worker.js
    e

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "guala-cache-v2";
+const CACHE_NAME = "guala-cache-v3";
+
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "/segnalazioni-guala/",
+  "/segnalazioni-guala/index.html",
+  "/segnalazioni-guala/manifest.json",
+  "/segnalazioni-guala/icons/icon-192.png",
+  "/segnalazioni-guala/icons/icon-512.png"
 ];
 
 // INSTALL
@@ -15,9 +16,25 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
+// ACTIVATE (cancella cache vecchie)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
 // FETCH
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
   );
 });
