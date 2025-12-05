0953a3864cbb347b73fba96f7f002bd4ae3fd4eb
ivo23975-art <ivo23975@gmail.com>
2025-12-05 22:54:51 +0100

    Update firebase-config.js
    30

--- dashboard/firebase-config.js
+++ dashboard/firebase-config.js
@@ -1,12 +1,15 @@
-// Firebase Config (identico al tuo index)
+// Firebase Config CORRETTO per Firestore
 const firebaseConfig = {
     apiKey: "AIzaSyAneaPGbeUWMNurMzGghrOoYpw4dsuIwr4",
     authDomain: "segnalazioni-guala.firebaseapp.com",
     projectId: "segnalazioni-guala",
-    storageBucket: "segnalazioni-guala.firebasestorage.app",
+    storageBucket: "segnalazioni-guala.appspot.com",  // CORRETTO
     messagingSenderId: "118462666966",
     appId: "1:118462666966:web:9ca2cdb6985581fe79d177"
 };
 
+// Inizializzazione Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Inizializzazione Firestore
 const db = firebase.firestore();
